7913a9191b9b53659a668fb861f0922b6f98dae3
Erisvaldo Carvalho Silva <erisvaldocarvalhosilva@gmail.com>
2023-09-18 13:26:49 -0300

    Criação da DepartamentoProdutoBLL

---
+++ Solution/BLL/DepartamentoProdutoBLL.cs
@@ -0,0 +1,45 @@
+﻿using DAL;
+using Models;
+
+namespace BLL
+{
+    public class DepartamentoProdutoBLL
+    {
+        public void Inserir(DepartamentoProduto _departamentoProduto)
+        {
+            using (DataContext dataContext = new DataContext())
+            {
+                dataContext.Add(_departamentoProduto);
+                dataContext.SaveChanges();
+            }
+        }
+        public void Alterar(DepartamentoProduto _departamentoProduto)
+        {
+            using (DataContext dataContext = new DataContext())
+            {
+                dataContext.Update(_departamentoProduto);
+                dataContext.SaveChanges();
+            }
+        }
+        public void Excluir(int _id)
+        {
+            using (DataContext dataContext = new DataContext())
+            {
+                dataContext.Remove(BuscarPorId(_id));
+                dataContext.SaveChanges();
+            }
+        }
+        public List<DepartamentoProduto> BuscarTodos()
+        {
+            return new DataContext().DepartamentoProduto.ToList();
+        }
+        public DepartamentoProduto BuscarPorId(int _id)
+        {
+            return new DataContext().DepartamentoProduto.Where(item => item.Id == _id).SingleOrDefault();
+        }
+        public List<DepartamentoProduto> BuscarDescricao(string _descricao)
+        {
+            return new DataContext().DepartamentoProduto.Where(item => item.Descricao.Contains(_descricao)).ToList();
+        }
+    }
+}
